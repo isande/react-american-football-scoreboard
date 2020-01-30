740efe49aa0403c87a3940a00841e38ea39695cd
Peggy Wollenhaupt <color.ui>
2020-01-29 18:57:24 -0700

    buttons for bottom row

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const BottomRow = () => {
-  const quarter = 1;
+function BottomRow() {
+
+  const [down, setDown] = useState(0);
+  const [toGo, setToGo] = useState(0);
+  const [ballOn, setBallOn] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+
+
 
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
+        <button onClick={() => setDown(down +1)} className="awayButtons__fieldGoal">+</button>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{toGo}</div>
+        <button onClick={() => setToGo(toGo +1)} className="awayButtons__fieldGoal">+</button>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{ballOn}</div>
+        <button onClick={() => setBallOn(ballOn +1)} className="awayButtons__fieldGoal">+</button>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
         <div className="quarter__value">{quarter}</div>
+        <button onClick={() => setQuarter(quarter + 1)} className="awayButtons__fieldGoal">+</button>
       </div>
     </div>
   );
